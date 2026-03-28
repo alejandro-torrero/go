e47a16492373a72bc91fbd10be861c42e39f4deb
Alejandro Torrero <alejandro@xira.ai>
2026-03-28 14:28:19 -0600

    feat: fmt scann  on calculator

--- calculator/investment_caculator.go
+++ calculator/investment_caculator.go
@@ -7,13 +7,20 @@ import (
 
 func main() {
 	const inflationRate = 2.5
-	var investmentAmount = 1000.0
-	expectedReturnRate := 5.5
-	years := 10.0
+	var investmentAmount float64
+	var expectedReturnRate float64
+	var years float64
+
+	fmt.Print("Insert investment amount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("Insert expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Insert years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow((1+(expectedReturnRate/100)), years)
 	futureRealvalue := futureValue / math.Pow(1+(inflationRate/100), years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealvalue)
+	fmt.Println("Future Value: ", futureValue)
+	fmt.Println("Future real value: ", futureRealvalue)
 }
